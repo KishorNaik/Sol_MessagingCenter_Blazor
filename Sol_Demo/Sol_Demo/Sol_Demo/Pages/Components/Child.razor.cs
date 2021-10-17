bcc00044d0bb30be91fcc549614ef4f845cabdf4
Kishor Naik <kishor.naik011.net@gmail.com>
2021-10-17 12:12:46 +0530

    Code Commit

---
+++ Sol_Demo/Sol_Demo/Sol_Demo/Pages/Components/Child.razor.cs
@@ -0,0 +1,16 @@
+﻿using AKSoftware.Blazor.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sol_Demo.Pages.Components
+{
+    public partial class Child
+    {
+        private void OnSendDataFromChildToParent()
+        {
+            MessagingCenter.Send<Child, String>(this, "Message", "Hello from Child");
+        }
+    }
+}
---
+++ Sol_Demo/Sol_Demo/Sol_Demo/Pages/Components/Parent.razor.cs
@@ -0,0 +1,31 @@
+﻿using AKSoftware.Blazor.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sol_Demo.Pages.Components
+{
+    public partial class Parent
+    {
+        private string MyMessage { get; set; }
+
+        private void SubscribeToMessage()
+        {
+            MessagingCenter.Subscribe<Child, string>(this, "Message", (sender, value) =>
+             {
+                 MyMessage = value;
+                 base.StateHasChanged();
+             });
+        }
+
+        protected override void OnAfterRender(bool firstRender)
+        {
+            if (firstRender)
+            {
+                SubscribeToMessage();
+                base.StateHasChanged();
+            }
+        }
+    }
+}
